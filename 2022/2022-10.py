b8cf7d0e788de83b418d7179125a61c9b95c6e3a
Sreeram Ramachandran <gro.mareers@sreeram.org>
2022-12-30 14:23:11 -0800

    2022-10 Cathode-Ray Tube: Add comments.

--- 2022/2022-10.py
+++ 2022/2022-10.py
@@ -5,6 +5,12 @@
 
 import itertools, lib.ocr
 
+# We exploit the fact that the input tokens ('noop', 'addx', number) match up
+# exactly with their clock cycles and with their respective effect on the value
+# of X (treating non-numbers as adding 0 to X). We also add a couple of entries
+# to the beginning of the sequence so that the index of an element matches the
+# cycle _during_ which X's value is the element.
+
 X = list(itertools.accumulate([0, 1] + [int(n) if n[-1].isdigit() else 0 for n in open(0).read().split()]))
 print(sum(i * n for i, n in list(enumerate(X))[20::40]))
 crt = ["".join("#" if abs(c - X[r * 40 + c + 1]) <= 1 else "." for c in range(40)) for r in range(6)]
