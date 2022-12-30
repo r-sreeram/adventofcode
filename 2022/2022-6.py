0e133564fe94053f1ead16e03005bdc91cacda33
Sreeram Ramachandran <gro.mareers@sreeram.org>
2022-12-30 13:22:22 -0800

    2022-6 (Tuning Trouble): Added comments.

--- 2022/2022-6.py
+++ 2022/2022-6.py
@@ -1,6 +1,9 @@
 # Day 6: Tuning Trouble
 # https://adventofcode.com/2022/day/6
 
+# Note the slightly unusual `len(...) + 1` as the end of the range(), so that
+# `[i - n : i]` will correctly go up to the last n chars.
+
 stream = input()
 pos = lambda n: next(i for i in range(n, len(stream) + 1) if len(set(stream[i - n : i])) == n)
 print(pos(4))
