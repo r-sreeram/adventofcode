7e6039fd47e3689a53e37864697e4654b87a7229
Sreeram Ramachandran <gro.mareers@sreeram.org>
2022-12-30 11:33:59 -0800

    2022-4 (Camp Cleanup): Add comments.

--- 2022/2022-4.py
+++ 2022/2022-4.py
@@ -3,6 +3,21 @@
 
 import re
 
+# For Part 1, we want interval pairs where one fully contains the other, which
+# means one of the following situations (along the horizontal axis):
+#      a1 o-------------o a2            a1 o-o a2
+#            b1 o-o b2            b1 o-------------o b2
+# <=> (a1 <= b1 and b2 <= a2) or (b1 <= a1 and a2 <= b2)
+
+# For Part 2, we want interval pairs which have at least some overlap, which
+# means NEITHER of the following situations (along the horizontal axis):
+#   a1 o-o a2   b1 o-o b2  or  b1 o-o b2   a1 o-o a2
+# <=> NOT (a2 < b1         or         b2 < a1)
+# <=> (a2 >= b1 and b2 >= a1)
+
+# We are assuming that in the input, a <= b for each interval (a, b).
+
 sections = [[int(id) for id in re.findall("\d+", pairs)] for pairs in open(0)]
+assert all(a1 <= a2 and b1 <= b2 for a1, a2, b1, b2 in sections)
 print(sum(a1 <= b1 and b2 <= a2 or b1 <= a1 and a2 <= b2 for a1, a2, b1, b2 in sections))
 print(sum(a2 >= b1 and b2 >= a1 for a1, a2, b1, b2 in sections))
