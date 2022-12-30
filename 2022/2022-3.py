68841ed7342a5ccba7167948331a0190a1d8dea1
Sreeram Ramachandran <gro.mareers@sreeram.org>
2022-12-30 10:35:48 -0800

    Add comments for 2022-3 (Rucksack Reorganization).

--- 2022/2022-3.py
+++ 2022/2022-3.py
@@ -3,6 +3,16 @@
 
 import string
 
+# The input is guaranteed to have exactly one letter common to all "parts"
+# (for Part 1, the parts are the two halves of each line; for Part 2, the parts
+# are groups of three lines). So, we can intersect the parts and blindly pop()
+# an element from the resulting set (there should be exactly one element).
+
+# We use string.ascii_letters for two purposes: (1) to find the index of the
+# letter (to compute the "priority"), and (2) as a base for the intersection
+# (otherwise, we'd have to explicitly create a set() from at least one part and
+# then intersect with the rest of the parts, which is less elegant to code).
+
 priority = lambda parts: string.ascii_letters.index(set(string.ascii_letters).intersection(*parts).pop()) + 1
 sacks = open(0).read().split()
 print(sum(priority((s[0 : len(s) // 2], s[len(s) // 2 :])) for s in sacks))
