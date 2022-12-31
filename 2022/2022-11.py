db24505fe3c4b27447c8195a865a18ec834b5684
Sreeram Ramachandran <gro.mareers@sreeram.org>
2022-12-31 13:31:47 -0800

    2022-11: Don't use modular arithmetic when division is involved.
    Earlier, the code computed the worry level for both parts like so:
        worry = (ops[m](item) // divisor) % LCM
    
    For Part 2, this is fine, since the divisor = 1 (i.e., nothing is
    actually getting divided).
    
    For Part 1 however, the divisor = 3, so it's wrong. Division is NOT
    compatible under modular arithmetic, except in specific circumstances.
    References:
        https://en.wikipedia.org/wiki/Modular_arithmetic#Properties
        https://www.doc.ic.ac.uk/~mrh/330tutor/ch03.html
    
    I realized this as I was writing comments on why modular arithmetic is
    provably correct for Part 2. I couldn't prove it for Part 1!
    
    I hadn't discovered the issue earlier, because the wrong code above
    still produces the correct answer for the AoC inputs (both example and
    real inputs), by sheer luck. The LCM in those inputs is so large, and
    "x // 3" is always small enough that the "% LCM" is a no-op.
    
    That gives us a hint on how to construct a counter-example:
        Monkey 0:
          Starting items: 209
          Operation: new = old + 1
          Test: divisible by 2
            If true: throw to monkey 1
            If false: throw to monkey 2
    
        Monkey 1:
          Starting items: 1
          Operation: new = old + 1
          Test: divisible by 5
            If true: throw to monkey 2
            If false: throw to monkey 0
    
        Monkey 2:
          Starting items: 1
          Operation: new = old + 1
          Test: divisible by 7
            If true: throw to monkey 0
            If false: throw to monkey 1
    
    In the above example, LCM = 2 * 5 * 7 = 70. When Monkey 0 processes the
    first item (209), consider the two versions of the code:
        Right: worry = ((209 + 1) // 3)       = 70
        Wrong: worry = ((209 + 1) // 3) % 70  =  0
    Regardless, the divisibility test gives the same result (both 70 and 0
    are divisible by 2), so the item is thrown to Monkey 1. But then see
    what happens when Monkey 1 processes the item:
        Right: worry = ((70 + 1) // 3)       = 23
        Wrong: worry = (( 0 + 1) // 3) % 70  =  0
    Now the divisibility test gives different results!

--- 2022/2022-11.py
+++ 2022/2022-11.py
@@ -3,6 +3,35 @@
 
 import copy, itertools, math
 
+# Straight-forward simulation of the throws, using the LCM of the divisors to
+# keep the "worry level" from growing out of control.
+
+# Say w is the worry level and N is the LCM. Why does it work to use w % N
+# instead of w itself in all the calculations?
+
+# What matters is the divisibility test. That is the only time the worry level
+# has a consequence (depending on the outcome, the item will be thrown to one
+# monkey or another). Apart from that, the worry level values aren't actually
+# used anywhere else. So, if we can prove that using w % N gives the same result
+# for the divisibility test as using w itself, we are good.
+
+# Say m is some specific monkey's divisor (for the divisibility test). We shall
+# prove that w = a (mod N) => w = a (mod m). I.e., ((w % N) % m) = a assures us
+# that (w % m) = a. Proof: Let w = a (mod N). This means w = N * k + a for some
+# integer k. Since N is a multiple of m, we have w = m * p * k + a, for some
+# integer p (i.e., N = m * p). Thus, w = a (mod m). Q.E.D.
+
+# How about the various operations (new = old * 26 and such)? Do they preserve
+# this property? Yes. Addition (which includes subtraction) and multiplication
+# are compatible under a modulus. E.g., w = a (mod m) => w * b = a * b (mod m).
+# See: https://en.wikipedia.org/wiki/Modular_arithmetic#Properties
+
+# This is not always true for division! w = a (mod m) => w / b = a / b (mod m)
+# is true if and only if b is coprime to m. Luckily, the input only has addition
+# and multiplication operations (no "new = old / 26"), so we are good. Part 1
+# does involve division (by 3), so we _cannot_ use modular arithmetic there. See
+# the commit description for an example where using it gives the wrong answer.
+
 items, ops, mods, targets = [], [], [], []
 for monkey in open(0).read().split("\n\n"):
     lines = monkey.splitlines()
@@ -16,19 +45,18 @@ for monkey in open(0).read().split("\n\n"):
         ]
     )
 
-N = math.lcm(*mods)
 
-
-def solve(items, num_rounds, divisor):
+def solve(items, num_rounds, reduce):
     counts = [0] * len(items)
     for _, m in itertools.product(range(num_rounds), range(len(items))):
         counts[m] += len(items[m])
         for item in items[m]:
-            worry = ops[m](item) // divisor % N
+            worry = reduce(ops[m](item))
             items[targets[m][worry % mods[m] == 0]].append(worry)
         items[m] = []
     print(math.prod(sorted(counts)[-2:]))
 
 
-solve(copy.deepcopy(items), 20, 3)
-solve(items, 10000, 1)
+solve(copy.deepcopy(items), 20, lambda x: x // 3)
+lcm = math.lcm(*mods)
+solve(items, 10000, lambda x: x % lcm)
