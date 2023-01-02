d80381a0e9609d11a602f5f628a35503aedce63d
Sreeram Ramachandran <gro.mareers@sreeram.org>
2023-01-01 22:42:20 -0800

    2022-23: Add comments.

--- 2022/2022-23.py
+++ 2022/2022-23.py
@@ -3,21 +3,35 @@
 
 # Inspired by dcclct13: https://www.reddit.com/r/adventofcode/comments/zt6xz5/2022_day_23_solutions/j1dq8oj/
 
+# This is a simple simulation of the puzzle rules, using a neat observation that
+# if multiple elves propose moving to the same spot, they must be coming at it
+# from opposite directions, and there can only be two such elves (for each
+# target position).
+
+# In each row r, DIRS[r][1] is the actual direction the elf will move, if all
+# the positions indicated by DIRS[r][:] are unoccupied.
 DIRS = [(-1 - 1j, -1j, 1 - 1j), (-1 + 1j, 1j, 1 + 1j), (-1 - 1j, -1, -1 + 1j), (1 - 1j, 1, 1 + 1j)]
 
 grove = {c + r * 1j for r, row in enumerate(open(0)) for c, char in enumerate(row) if char == "#"}
-turn, moves = 0, 1
-while moves:
-    turn, moves = turn + 1, {}
+turn, plan = 0, 1
+while plan:
+    # plan is a map of valid proposals. If an elf currently at position p is
+    # proposing a move to position q, then plan[q] = p. If multiple elves
+    # propose to move to the same position, plan will contain none of them (by
+    # the end of the loop).
+    turn, plan = turn + 1, {}
     for elf in grove:
         if any(elf + dir in grove for dir in (-1 - 1j, -1j, 1 - 1j, -1, 1, -1 + 1j, 1j, 1 + 1j)):
             for dir in DIRS:
                 if all(elf + d not in grove for d in dir):
-                    if (prev := moves.pop(elf + dir[1], None)) == None:
-                        moves[elf + dir[1]] = elf
+                    # If a previous elf had already proposed moving to the same
+                    # position (elf + dir[1]), remove it from the plan, and
+                    # don't add us either. If not, add ourselves to the plan.
+                    if (prev := plan.pop(elf + dir[1], None)) == None:
+                        plan[elf + dir[1]] = elf
                     break
-    grove.difference_update(moves.values())
-    grove.update(moves.keys())
+    grove.difference_update(plan.values())
+    grove.update(plan.keys())
     DIRS.append(DIRS.pop(0))
     if turn == 10:
         x1, y1 = min(elf.real for elf in grove), min(elf.imag for elf in grove)
