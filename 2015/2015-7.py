32f240354eef3dc1f620ce8ad2d275fc53df6386
Sreeram Ramachandran <gro.mareers@sreeram.org>
2023-01-03 09:48:37 -0800

    2015-7: Compactify the code.

--- 2015/2015-7.py
+++ 2015/2015-7.py
@@ -1,37 +1,27 @@
 # Day 7: Some Assembly Required
 # https://adventofcode.com/2015/day/7
 
-import functools, operator
+import functools, operator as op
 
-OPS = {"AND": operator.and_, "OR": operator.or_, "LSHIFT": operator.lshift, "RSHIFT": operator.rshift}
+OPS = {"AND": op.and_, "OR": op.or_, "LSHIFT": op.lshift, "RSHIFT": op.rshift, "NOT": op.inv, "X": lambda x: x}
 
 wires = {}
 for line in open(0):
     match line.split():
         case x, "->", w:
-            wires[w] = x
-        case "NOT", x, "->", w:
-            wires[w] = [x, operator.inv]
-        case x, op, y, "->", w:
-            wires[w] = [x, y, OPS[op]]
+            wires[w] = (OPS["X"], x)
+        case *lhs, operation, rhs, "->", w:
+            wires[w] = (OPS[operation], *lhs, rhs)
 
 
 @functools.cache
 def solve(w):
     if isinstance(w, int) or w[0].isdigit():
         return int(w) & 0xFFFF
-    val = wires[w]
-    if isinstance(val, list):
-        if len(val) == 2:
-            val = val[1](solve(val[0]))
-        else:
-            val = val[2](solve(val[0]), solve(val[1]))
-    else:
-        val = solve(val)
-    return val & 0xFFFF
+    return wires[w][0](*[solve(arg) for arg in wires[w][1:]]) & 0xFFFF
 
 
 print(part1 := solve("a"))
-wires["b"] = part1
+wires["b"] = (OPS["X"], part1)
 solve.cache_clear()
 print(solve("a"))
